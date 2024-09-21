7f852085a8c316f6f05b4bdbe1188ab464817669
kennedyjulius <kennedymutugi111@gmail.com>
2024-09-21 05:32:25 +0300

    first section

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:multi_vendor_app/constants/constants.dart';
+import 'package:multi_vendor_app/views/entrypoint.dart';
+
+
+Widget defaultHome = const MainScreen();
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+    return ScreenUtilInit(
+      designSize: const Size(375, 825),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_ , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          theme: ThemeData(
+            scaffoldBackgroundColor: kOffWhite,
+            iconTheme: IconThemeData(color: kDark),
+            primarySwatch: Colors.grey,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: child,
+        );
+      },
+      child: defaultHome,
+    );
+  }
+}
