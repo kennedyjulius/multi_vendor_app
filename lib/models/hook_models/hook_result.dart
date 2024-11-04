263c13b0e42602aae4f445fb13f296e59e519323
kennedyjulius <kennedymutugi111@gmail.com>
2024-11-04 05:36:24 +0300

    added some code

--- lib/models/hook_models/hook_result.dart
+++ lib/models/hook_models/hook_result.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:multi_vendor_app/models/api_error.dart';
+import 'package:multi_vendor_app/models/categories.dart';
 
 class FetchHook {
-  final dynamic data;
+  final List<CategoriesModel>? data;
   final bool isloading;
   final Exception? error;
   final VoidCallback? refetch;
@@ -10,6 +12,6 @@ class FetchHook {
     required this.data, 
     required this.isloading, 
     required this.error, 
-    required this.refetch
+    required this.refetch, required bool isLoading, ApiError? apiError
     });
 }
